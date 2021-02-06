e81b370b4eb423b20302a630e678b34acc243d0c
Mariusz Olszewski <ssssspam11@gmail.com>
2021-02-06 15:44:46 +0100

    Creating method returnElements

--- src/drawClass.js
+++ src/drawClass.js
@@ -16,7 +16,8 @@
 // ]
 
 class drawCards{
-    
+    deleteElement=0;
+    deleteElement2=0;
     firstCard=[0,1,2,3,4,5,6];
     secondCard=[7,8,9,10,11,12,13];
 
@@ -26,17 +27,17 @@ class drawCards{
         const randomElement = array[deleteClass];
         array.splice(deleteClass,1);
         
-        const deleteElement=Math.floor(Math.random() * this.firstCard.length);
-        const elem1=this.firstCard[deleteElement];
+        this.deleteElement=Math.floor(Math.random() * this.firstCard.length);
+        const elem1=this.firstCard[this.deleteElement];
         const elem2=document.querySelector(`.icon${elem1}`);
             elem2.classList.add(randomElement,"colored");
-        this.firstCard.splice(deleteElement,1);
+        this.firstCard.splice(this.deleteElement,1);
 
-        const deleteElement2=Math.floor(Math.random() * this.secondCard.length);
-        const elem3=this.secondCard[deleteElement2];
+        this.deleteElement2=Math.floor(Math.random() * this.secondCard.length);
+        const elem3=this.secondCard[this.deleteElement2];
         let elem4=document.querySelector(`.icon${elem3}`);
             elem4.classList.add(randomElement,"colored");
-            this.secondCard.splice(deleteElement2,1);
+        this.secondCard.splice(this.deleteElement2,1);
 
         let deck=this.firstCard.concat(this.secondCard);
 
@@ -44,13 +45,19 @@ class drawCards{
             let item=document.querySelector(`.icon${deck[i]}`);
             item.classList.add(array[i],"colored");
         }
-    }    
+
+    }
+    returnElemments(){
+        return this.deleteElement, this.deleteElement2;
+    }
+       
 }
 
 // let bla = new drawCards;
 // bla.draw(iconsClassArray);
 
 export default drawCards;
+
 
 
 
