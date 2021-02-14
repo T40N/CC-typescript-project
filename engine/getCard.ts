12568c0236c4b6f5908b51afc9c99a1b624532c0
Arleta Jędrzejczak <Arleta.Jedrzejczak@ttpsc.pl>
2021-02-14 17:58:14 +0100

    Prettier added

--- engine/getCard.ts
+++ engine/getCard.ts
@@ -1,19 +1,18 @@
-import {iconClasses} from './iconClasses'
+import { iconClasses } from "./iconClasses";
 
-function getCard(card: string[]){
-   const match=card[Math.floor(Math.random()*(card.length))]
-   let newCard=[]
+function getCard(card: string[]) {
+  const match = card[Math.floor(Math.random() * card.length)];
+  let newCard = [];
 
-   while(newCard.length<8){
-      let c=iconClasses[Math.floor(Math.random()*(iconClasses.length))]
+  while (newCard.length < 8) {
+    let c = iconClasses[Math.floor(Math.random() * iconClasses.length)];
 
-      if(c!=match && newCard.indexOf(c)==-1)
-         newCard.push(c)
-   }
+    if (c != match && newCard.indexOf(c) == -1) newCard.push(c);
+  }
 
-   newCard[Math.floor(Math.random()*(newCard.length))]=match
+  newCard[Math.floor(Math.random() * newCard.length)] = match;
 
-   return {match: match, items: newCard}
+  return { match: match, items: newCard };
 }
 
-export default getCard
+export default getCard;
