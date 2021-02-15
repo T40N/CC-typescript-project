fe9b3ef91693a8ddad33c7d0f724c2810e00d795
Stanislaw09 <sgardzielewski09@gmail.com>
2021-02-15 15:48:45 +0100

    tests & prettier

--- ts-build/main.test.js
+++ ts-build/main.test.js
@@ -1,46 +1,40 @@
-const {getRandomCard, getCard}=require('./main')
+const { getRandomCard, getCard } = require("./main");
 
-const beUnique=(a)=>{
-   let arr=a
-   arr.sort()
+const beUnique = (a) => {
+  let arr = a;
+  arr.sort();
 
-   for(let i=0;i<arr.length-1;i++)
-      if(arr[i]==arr[i-1])
-         return false
-   
-   return true
-}
+  for (let i = 0; i < arr.length - 1; i++)
+    if (arr[i] == arr[i - 1]) return false;
 
-const oneMatch=(card1, card2, match)=>{
-   let found=0
+  return true;
+};
 
-   if(card1.length!==card2.length)
-      return false
+const oneMatch = (card1, card2, match) => {
+  let found = 0;
 
-   if(card1.indexOf(match)===-1 || card2.indexOf(match)===-1)
-      return false
+  if (card1.length !== card2.length) return false;
 
-   for(let i=0;i<card1.length;i++)
-      if(card2.indexOf(card1[i])!==-1 && card1[i]!==match)
-         return false
+  if (card1.indexOf(match) === -1 || card2.indexOf(match) === -1) return false;
 
-   return true
-}
+  for (let i = 0; i < card1.length; i++)
+    if (card2.indexOf(card1[i]) !== -1 && card1[i] !== match) return false;
 
-test('getting proper card length', ()=>{
-   expect(getRandomCard(8)).toHaveLength(8)
-})
+  return true;
+};
 
-test('getting unique cards', ()=>{
-   let arr=getRandomCard()
-   expect(beUnique(arr)).toBeTruthy()
-})
+test("getting proper card length", () => {
+  expect(getRandomCard(8)).toHaveLength(8);
+});
 
-test('matching cards',()=>{
-   let card1=getRandomCard(8)
-   let newCard=getCard(card1)
+test("getting unique cards", () => {
+  let arr = getRandomCard();
+  expect(beUnique(arr)).toBeTruthy();
+});
 
-   console.log(card1, newCard);
+test("matching cards", () => {
+  let card1 = getRandomCard(8);
+  let newCard = getCard(card1);
 
-   expect(oneMatch(card1, newCard.items, newCard.match)).toBeTruthy()
-})
+  expect(oneMatch(card1, newCard.items, newCard.match)).toBeTruthy();
+});
